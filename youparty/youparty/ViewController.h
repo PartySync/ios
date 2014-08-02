a736eb7bff1ecf1450049c185d4a36b7de48aef2
Kevin Frans <kevin@bobthechicken.com>
2014-08-02 12:33:31 -0700

    added cocoa pods

---
+++ youparty/youparty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  youparty
+//
+//  Created by Kevin Frans on 8/2/14.
+//  Copyright (c) 2014 Kevin Frans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
